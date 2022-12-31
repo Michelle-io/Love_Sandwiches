141611c89b30bba1ceb0aedf2dcd8e606bc2dc7e
Michelle McCann <105986556+Michelle-io@users.noreply.github.com>
2022-12-31 11:59:17 +0000

    bug fix

--- run.py
+++ run.py
@@ -1,7 +1,6 @@
 import gspread
 from google.oauth2.service_account import Credentials
 
-
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
@@ -26,7 +25,7 @@ def get_sales_data():
         print("Data should be six numbers, separated by commas.")
         print("Example: 10,20,30,40,50,60\n")
 
-        data_str = input("Enter your data here: \n")
+        data_str = input("Enter your data here:\n")
 
         sales_data = data_str.split(",")
 
@@ -104,7 +103,7 @@ def get_last_5_entries_sales():
 
 def calculate_stock_data(data):
     """
-    calcuate the average stock for each item type, adding 10%
+    Calculate the average stock for each item type, adding 10%
     """
     print("Calculating stock data...\n")
     new_stock_data = []
@@ -115,6 +114,7 @@ def calculate_stock_data(data):
         stock_num = average * 1.1
         new_stock_data.append(round(stock_num))
 
+    return new_stock_data
 
 
 def main():
